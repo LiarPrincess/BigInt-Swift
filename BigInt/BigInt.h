c4c0c8bd91c93d43b4de66225b12c99407aab7df
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-05-21 11:00:58 +0200

    Initial Commit

---
+++ BigInt/BigInt.h
@@ -0,0 +1,19 @@
+//
+//  BigInt.h
+//  BigInt
+//
+//  Created by Michal Matuszczyk on 21/05/2020.
+//  Copyright © 2020 NoPoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BigInt.
+FOUNDATION_EXPORT double BigIntVersionNumber;
+
+//! Project version string for BigInt.
+FOUNDATION_EXPORT const unsigned char BigIntVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigInt/PublicHeader.h>
+
+
