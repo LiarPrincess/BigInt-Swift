fe673b79575745ba1d80a759b0ac8d92bed3339e
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-06-10 18:10:36 +0200

    Better performance for 'allPossiblePairings'

--- Tests/CoreTests/Helpers/GlobalFunctions.swift
+++ Tests/CoreTests/Helpers/GlobalFunctions.swift
@@ -1,23 +1,57 @@
 // MARK: - Pair values
 
-internal func allPossiblePairings<T>(values: [T]) -> [(T, T)] {
-  var result = [(T, T)]()
-  result.reserveCapacity(values.count * values.count)
+internal struct PossiblePairings<T>: Sequence {
 
-  for lhs in values {
-    for rhs in values {
-      let pair = (lhs, rhs)
-      result.append(pair)
+  internal typealias Element = (T, T)
+
+  internal struct Iterator: IteratorProtocol {
+    private var lhsIndex = 0
+    private var rhsIndex = 0
+    private let values: [T]
+
+    fileprivate init(values: [T]) {
+      self.values = values
+    }
+
+    internal mutating func next() -> Element? {
+      if self.lhsIndex == self.values.count {
+        return nil
+      }
+
+      let lhs = self.values[self.lhsIndex]
+      let rhs = self.values[self.rhsIndex]
+
+      self.rhsIndex += 1
+      if self.rhsIndex == self.values.count {
+        self.lhsIndex += 1
+        self.rhsIndex = 0
+      }
+
+      return (lhs, rhs)
     }
   }
 
-  return result
+  private let values: [T]
+
+  fileprivate init(values: [T]) {
+    self.values = values
+  }
+
+  internal func makeIterator() -> Iterator {
+    return Iterator(values: self.values)
+  }
+}
+
+/// `[1, 2] -> [(1,1), (1,2), (2,1), (2,2)]`
+internal func allPossiblePairings<T>(values: [T]) -> PossiblePairings<T> {
+  return PossiblePairings(values: values)
 }
 
 // MARK: - Powers of 2
 
 internal typealias PowerOf2<T> = (power: Int, value: T)
 
+/// `1, 2, 4, 8, 16, 32, 64, 128, 256, 512, etc...`
 internal func allPositivePowersOf2<T: FixedWidthInteger & BinaryInteger>(
   type: T.Type
 ) -> [PowerOf2<T>] {
@@ -32,6 +66,7 @@ internal func allPositivePowersOf2<T: FixedWidthInteger & BinaryInteger>(
   return result
 }
 
+/// `-1, -2, -4, -8, -16, -32, -64, -128, -256, -512, etc...`
 internal func allNegativePowersOf2<T: FixedWidthInteger & BinaryInteger>(
   type: T.Type
 ) -> [PowerOf2<T>] {
